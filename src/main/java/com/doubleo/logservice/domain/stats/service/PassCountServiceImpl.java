a8c23d3418fb5bc9ce32484e0629493847815ddc
coffeesigma <ljb22060@gmail.com>
2025-06-09 16:33:10 +0900

    spotless

--- src/main/java/com/doubleo/logservice/domain/stats/service/PassCountServiceImpl.java
+++ src/main/java/com/doubleo/logservice/domain/stats/service/PassCountServiceImpl.java
@@ -5,10 +5,9 @@ import com.doubleo.logservice.domain.stats.dto.response.PassCountInfoResponse;
 import com.doubleo.logservice.domain.stats.repository.IssuedLogQueryRepository;
 import com.doubleo.logservice.global.enums.VisitCategory;
 import com.doubleo.logservice.global.util.TenantValidator;
+import com.doubleo.logservice.grpc.client.AreaClient;
 import java.time.LocalDate;
 import java.util.*;
-
-import com.doubleo.logservice.grpc.client.AreaClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
