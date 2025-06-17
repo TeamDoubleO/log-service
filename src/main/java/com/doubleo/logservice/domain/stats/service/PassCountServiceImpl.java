622d8399aceef98166d5b1ad7eaf35d20e5c332b
midday2612 <midday2612@gmail.com>
2025-06-17 15:08:10 +0900

    KW-693/feat: 대시보드 mock data api

--- src/main/java/com/doubleo/logservice/domain/stats/service/PassCountServiceImpl.java
+++ src/main/java/com/doubleo/logservice/domain/stats/service/PassCountServiceImpl.java
@@ -55,4 +55,53 @@ public class PassCountServiceImpl implements PassCountService {
 
         return result;
     }
+
+    @Override
+    public List<PassCountInfoResponse> getMockPassCount() {
+        String[] areaCodes = {"AA_01_01", "AA_02_01", "AB_01_02", "AC_02_02"};
+        String[] areaNames = {"수면센터", "응급실", "의무기록실", "소아과"};
+        int[][] counts = {
+            {389, 1140, 160, 819},
+            {342, 1251, 173, 890},
+            {385, 1254, 160, 874},
+            {379, 1165, 146, 857},
+            {381, 1180, 163, 840},
+            {355, 1119, 179, 836},
+            {390, 1142, 165, 928},
+            {396, 1204, 187, 899},
+            {367, 1274, 177, 995},
+            {345, 1273, 158, 871},
+            {387, 1167, 161, 903},
+            {343, 1167, 172, 844},
+            {352, 1223, 180, 857},
+            {395, 1260, 174, 952},
+            {398, 1259, 177, 847},
+            {358, 1136, 153, 966},
+            {386, 1121, 176, 952},
+            {391, 1128, 157, 910},
+            {348, 1205, 180, 878},
+            {358, 1213, 144, 931},
+            {387, 1232, 174, 961},
+            {360, 1283, 185, 988},
+            {382, 1292, 165, 944},
+            {349, 1294, 142, 822},
+            {358, 1157, 171, 954},
+            {389, 1114, 180, 989},
+            {376, 1127, 178, 930},
+            {348, 1292, 144, 838}
+        };
+
+        List<PassCountInfoResponse> result = new ArrayList<>();
+        LocalDate start = LocalDate.now().minusDays(28);
+
+        for (int i = 0; i < counts.length; i++) {
+            for (int j = 0; j < areaCodes.length; j++) {
+                result.add(
+                        new PassCountInfoResponse(
+                                start.plusDays(i), areaCodes[j], areaNames[j], counts[i][j]));
+            }
+        }
+
+        return result;
+    }
 }
